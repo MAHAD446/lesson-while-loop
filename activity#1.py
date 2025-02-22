335457920bca98c1fda2cbe27aa6d72bb31a61b8
mahad446 <mahadmubashir43@gmail.com>
2025-02-22 14:22:12 -0800

    activity#1

---
+++ activity#1.py
@@ -0,0 +1,11 @@
+#input thw value of terms
+n = int(input("enter the values of terms"))
+
+
+sum = 0 #initialse
+i = 1 #initialise
+while i<=n: #loop will run from 1 to n
+    sum = sum+i
+    i = i+1
+
+print("/nsum =", sum)
